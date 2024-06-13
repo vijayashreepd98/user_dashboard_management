00b2576da8bbed5442d61d16e6e2e959e4d0d8b9
vijayashreepd98@gmail.com <vijayashreepd98@gmail.com>
2024-06-13 21:39:19 +0530

    fixes on cms

--- src/global/constant.ts
+++ src/global/constant.ts
@@ -1,7 +1,7 @@
 const API_END_POINT =
-  "http://localhost:3000/";
+  "https://ep9jv47c0d.execute-api.us-east-1.amazonaws.com/dev/";
 const STATUS_CODES = {
-    NOT_FOUND:404,
-    SUCCESS:200,
-}
-export { API_END_POINT , STATUS_CODES};
+  NOT_FOUND: 404,
+  SUCCESS: 200,
+};
+export { API_END_POINT, STATUS_CODES };
